8e345f40e6191fe56fce1fdaa749a4e1772946e8
HigorAntonio <higorantonio.ha@gmail.com>
2021-01-27 22:36:51 -0300

    SideBar column

---
+++ client/src/components/SideBar/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import StickyBox from 'react-sticky-box';
+
+import List from '../List';
+import FollowSugestion from '../FollowSugestion';
+import News from '../News';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <StickyBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSugestion name="UserName1" nickname="@username1" />,
+              <FollowSugestion name="UserName2" nickname="@username2" />,
+              <FollowSugestion name="UserName3" nickname="@username3" />,
+            ]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+        </Body>
+      </StickyBox>
+    </Container>
+  );
+};
+
+export default SideBar;
